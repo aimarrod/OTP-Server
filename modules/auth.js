e7f9b9f3f20b4809aeba098b20b2661c3cdc33dc
beeg <bego_sope92@hotmail.com>
2013-06-05 21:13:27 +0200

    truncate method added

--- modules/auth.js
+++ modules/auth.js
@@ -8,9 +8,17 @@ module.exports.check = function(user, res){
 		//Generate hmac and all the process.
 		console.log(user);
 		var hc = crypto.createHmac('sha1', user.passphrase).update(user.counter).digest('hex');
-		//
+		hex = [];
+		//Transform into hexadecimal
+		for (var i = 0; i < hc.length; i += 2) {
+		    hex.push("0x" + hc.substr(i, 2));
+		}
 		//Metodo de truncado
-		//
-		res.end(hc);
+		offset = hex[hex.length-1];
+		offset = parseInt(offset,16);		
+		var base = (parseInt(hex[offeset], 16) + parseInt(hex[offeset+1], 16) + parseInt(hex[offeset+2], 16) + parseInt(hex[offeset+3], 16)).toString(16);
+  		base = base & 0x7f;
+  		base = parseInt(base.substr(2), 16);
+		res.end(base % (10**digit));
 	});
 };
